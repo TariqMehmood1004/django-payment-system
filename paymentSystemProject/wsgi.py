41853c4fdeaea6176a4ba4fcb04781420e63718b
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2024-12-25 01:09:09 +0500

    updated vercel

--- paymentSystemProject/wsgi.py
+++ paymentSystemProject/wsgi.py
@@ -1,10 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paymentSystemProject.settings')
 
 application = get_wsgi_application()
-app = application
-
-
